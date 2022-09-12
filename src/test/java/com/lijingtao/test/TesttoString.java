853bb4d239158e0a468fb608db7a3de18a9ce371
ludde88 <liludde88@gmail.com>
2022-09-12 22:33:22 +0800

    qq

--- src/test/java/com/lijingtao/test/TesttoString.java
+++ src/test/java/com/lijingtao/test/TesttoString.java
@@ -7,8 +7,8 @@ import org.springframework.context.support.ClassPathXmlApplicationContext;
 public class TesttoString {
     @Test
     public void test(){
-        /*ClassPathXmlApplicationContext cp = new ClassPathXmlApplicationContext("applicationContext.xml");
+        ClassPathXmlApplicationContext cp = new ClassPathXmlApplicationContext("applicationContext.xml");
         Person person1 = (Person)cp.getBean("person");
-        System.out.println(person1.toString());*/
+        System.out.println(person1.toString());
     }
 }
